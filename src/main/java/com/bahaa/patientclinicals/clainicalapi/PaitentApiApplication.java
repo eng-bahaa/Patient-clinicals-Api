86e3c2390346019aacad35310b51147a98d43fc8
bahaeldin <bahaa.ahmed.eng@gmail.com>
2025-04-12 20:30:46 -0700

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/bahaa/patientclinicals/clainicalapi/PaitentApiApplication.java
@@ -0,0 +1,13 @@
+package com.bahaa.patientclinicals.clainicalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaitentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaitentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bahaa/patientclinicals/clainicalapi/PaitentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahaa.patientclinicals.clainicalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaitentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
