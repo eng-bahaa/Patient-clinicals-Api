85e45b2c23e475f6e78a09ebe77ce40a22890184
bahaeldin <bahaa.ahmed.eng@gmail.com>
2025-04-14 21:04:22 -0700

    Add Paitent Entity & edit pom.xml file

--- src/main/java/com/bahaa/patientclinicals/clainicalapi/models/Paitent.java
+++ src/main/java/com/bahaa/patientclinicals/clainicalapi/models/Paitent.java
@@ -1,8 +1,75 @@
 package com.bahaa.patientclinicals.clainicalapi.models;
 
-import javax.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "paitent")
 public class Paitent {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+
+	@Column(name = "last_name", nullable = false)
+	private String lastName;
+
+	@Column(name = "age", nullable = false)
+	private int age;
+
+	public Paitent() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Paitent(String firstName, String lastName, int age) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Paitent [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + "]";
+	}
 
 }
