00a12a70a995c803c781104d396e79f928b0e449
bahaeldin <bahaa.ahmed.eng@gmail.com>
2025-04-17 10:23:00 -0700

    Error fixesd and Repository added

--- src/main/java/com/bahaa/patientclinicals/clainicalapi/controllers/MyController.java
+++ src/main/java/com/bahaa/patientclinicals/clainicalapi/controllers/MyController.java
@@ -3,17 +3,12 @@ package com.bahaa.patientclinicals.clainicalapi.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bahaa.patientclinicals.clainicalapi.models.Patient;
-
 @RestController
 public class MyController {
-
-	private Patient paitent;
 
 	@GetMapping("/")
 	public String hello() {
 
-		paitent = new Patient("Bahaa", "Bahar", 22);
-		return "Bahaeldin Bahar + " + paitent.toString();
+		return "Bahaeldin Bahar";
 	}
 }
